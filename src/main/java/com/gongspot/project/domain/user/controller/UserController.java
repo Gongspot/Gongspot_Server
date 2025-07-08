6ca50b9b254bfd29baee0b6531f3b179800fdee1
jaehyeon4406 <jaehyeon4406@gmail.com>
2025-07-09 03:33:03 +0900

    feat: 닉네임 등록 기능 추가(자료형 수정)

--- src/main/java/com/gongspot/project/domain/user/controller/UserController.java
+++ src/main/java/com/gongspot/project/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.gongspot.project.domain.user.controller;
 
 import com.gongspot.project.common.response.ApiResponse;
 import com.gongspot.project.domain.user.dto.UserRequestDTO;
+import com.gongspot.project.domain.user.dto.UserResponseDTO;
 import com.gongspot.project.domain.user.service.UserCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,7 @@ public class UserController {
     private final UserCommandService userCommandService;
 
     @PostMapping("/nickname")
-    public ApiResponse<void> registerNickname(@RequestBody UserRequestDTO.NicknameRequestDTO request) {
+    public ApiResponse<UserResponseDTO.NicknameResponseDTO> registerNickname(@RequestBody UserRequestDTO.NicknameRequestDTO request) {
         Long userId = getCurrentUserId();
         userCommandService.registerNickname(userId, request.getNickname());
         return ApiResponse.onSuccess(null);
