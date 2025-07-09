aaeb5758437d23edbbffdf2cc54bcc0f1756ff43
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-07-09 15:26:29 +0900

    fix: 포인트 조회 로직 맞게 로그인 방식 수정

---
+++ src/main/java/com/gongspot/project/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gongspot.project.domain.user.repository;
+
+import com.gongspot.project.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
