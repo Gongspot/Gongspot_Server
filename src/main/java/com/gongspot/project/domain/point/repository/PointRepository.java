cde74484281db3ede17f5775f747f8492e30f805
wnsdud <cccjj3829@gmail.com>
2025-07-09 21:20:08 +0900

    포인트 내역 조회 api 구현

--- src/main/java/com/gongspot/project/domain/point/repository/PointRepository.java
+++ src/main/java/com/gongspot/project/domain/point/repository/PointRepository.java
@@ -4,8 +4,13 @@ import com.gongspot.project.domain.point.entity.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface PointRepository extends JpaRepository<Point, Long> {
 
     @Query("SELECT COALESCE(SUM(p.updatedPoint), 0) FROM Point p WHERE p.user.id = :userId")
     Long getTotalPointsByUserId(Long userId);
+
+    Page<Point> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
 }
