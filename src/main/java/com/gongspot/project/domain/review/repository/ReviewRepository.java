64d7bfbcd9d066a2f048cd11f562e951275bb0b4
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2025-07-09 21:47:46 +0900

    refactor : 리뷰 쿼리 수정 (Media 조인 제거 하고 Place직접참조)

--- src/main/java/com/gongspot/project/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/gongspot/project/domain/review/repository/ReviewRepository.java
@@ -12,16 +12,15 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("""
         SELECT AVG(r.rating)
-        FROM Media m
-        JOIN m.review r
-        WHERE m.place.id = :placeId
+        FROM Review r
+        WHERE r.place.id = :placeId
     """)
     Double getAverageRatingByPlaceId(@Param("placeId") Long placeId);
 
     @Query("""
-        SELECT r FROM Media m
-        JOIN m.review r
-        WHERE m.place.id = :placeId
+        SELECT r
+        FROM Review r
+        WHERE r.place.id = :placeId
         ORDER BY r.datetime DESC
     """)
     List<Review> findTop3ByPlaceId(@Param("placeId") Long placeId, Pageable pageable);
