a4e52c3b14cc598ad8ee828fd7abb0b62cd32017
wnsdud <cccjj3829@gmail.com>
2025-07-18 20:56:53 +0900

    ã새로운 공간 등록 api 구현

---
+++ src/main/java/com/gongspot/project/domain/newplace/controller/NewPlaceController.java
@@ -0,0 +1,27 @@
+package com.gongspot.project.domain.newplace.controller;
+
+import com.gongspot.project.common.response.ApiResponse;
+import com.gongspot.project.domain.newplace.dto.NewPlaceRequestDTO;
+import com.gongspot.project.domain.newplace.dto.NewPlaceResponseDTO;
+import com.gongspot.project.domain.newplace.service.NewPlaceCommandService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/places")
+@RequiredArgsConstructor
+public class NewPlaceController {
+
+    private final NewPlaceCommandService newPlaceCommandService;
+
+    @Operation(summary = "새 공간 등록 신청")
+    @PostMapping("/proposal")
+    public ApiResponse<NewPlaceResponseDTO> createNewPlaceProposal(
+            @RequestBody @Valid NewPlaceRequestDTO requestDTO
+    ) {
+        NewPlaceResponseDTO result = newPlaceCommandService.createNewPlaceProposal(requestDTO);
+        return ApiResponse.onSuccess(result);
+    }
+}
