6f0e45b4052611eae59d5769ebd57eb8fccc9646
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-07-20 18:12:00 +0900

    refactor: controller 사용하도록 변경, 닉네임 검증로직 수정

--- src/main/java/com/gongspot/project/domain/user/controller/UserController.java
+++ src/main/java/com/gongspot/project/domain/user/controller/UserController.java
@@ -3,8 +3,11 @@ package com.gongspot.project.domain.user.controller;
 import com.gongspot.project.common.response.ApiResponse;
 import com.gongspot.project.domain.user.dto.UserRequestDTO;
 import com.gongspot.project.domain.user.dto.UserResponseDTO;
+import com.gongspot.project.domain.user.entity.User;
 import com.gongspot.project.domain.user.service.UserCommandService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,27 +18,26 @@ public class UserController {
     private final UserCommandService userCommandService;
 
     // 닉네임 등록
+    @Operation(summary = "닉네임 등록 API", description = "사용자의 닉네임을 초기 등록합니다. (카카오톡 닉네임을 기본값으로 가져옵니다.)")
     @PostMapping("/nickname")
     public ApiResponse<UserResponseDTO.NicknameResponseDTO> registerNickname(
             @RequestBody UserRequestDTO.NicknameRequestDTO request
     ) {
-        Long userId = getCurrentUserId();
-        userCommandService.registerNickname(userId, request.getNickname());
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        userCommandService.registerNickname(user.getId(), request.getNickname());
         return ApiResponse.onSuccess(null);
     }
 
     // 프로필 수정(사진, 닉네임)
+    @Operation(summary = "프로필 수정(사진, 닉네임) API", description = "사진, 닉네임 등에 해당하는 프로필을 수정합니다. (기본값 카카오 프로필)")
     @PatchMapping("/profile")
-    public ApiResponse<UserResponseDTO.ProfileResponseDTO> updateProfile(@RequestBody UserRequestDTO.ProfileRequestDTO request) {
-        Long userId = getCurrentUserId();
-        UserResponseDTO.ProfileResponseDTO response = userCommandService.updateProfile(userId, request);
+    public ApiResponse<UserResponseDTO.ProfileResponseDTO> updateProfile(
+            @RequestBody UserRequestDTO.ProfileRequestDTO request
+    ) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        UserResponseDTO.ProfileResponseDTO response = userCommandService.updateProfile(user.getId(), request);
         return ApiResponse.onSuccess(response);
     }
-
-    // userId 변환
-    private Long getCurrentUserId() {
-        return 1L;
-    }
-
-
 }
--- src/main/java/com/gongspot/project/domain/user/service/UserCommandServiceImpl.java
+++ src/main/java/com/gongspot/project/domain/user/service/UserCommandServiceImpl.java
@@ -2,6 +2,7 @@ package com.gongspot.project.domain.user.service;
 
 import com.gongspot.project.common.code.status.ErrorStatus;
 import com.gongspot.project.common.exception.GeneralException;
+import com.gongspot.project.domain.user.converter.UserConverter;
 import com.gongspot.project.domain.user.dto.UserRequestDTO;
 import com.gongspot.project.domain.user.dto.UserResponseDTO;
 import com.gongspot.project.domain.user.entity.User;
@@ -35,18 +36,13 @@ public class UserCommandServiceImpl implements UserCommandService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new GeneralException(ErrorStatus.MEMBER_NOT_FOUND));
 
-        user.updateProfile(request.getNickname(), request.getProfileImg());
-
         if (!user.getNickname().equals(request.getNickname())
                 && userRepository.existsByNickname(request.getNickname())) {
             throw new GeneralException(ErrorStatus.DUPLICATE_NICKNAME);
         }
 
-        return UserResponseDTO.ProfileResponseDTO.builder()
-                .userId(user.getId())
-                .nickname(user.getNickname())
-                .profileImg(user.getProfileImg())
-                .build();
+        user.updateProfile(request.getNickname(), request.getProfileImg());
+        return UserConverter.toProfileResponseDTO(user);
     }
 
 }
