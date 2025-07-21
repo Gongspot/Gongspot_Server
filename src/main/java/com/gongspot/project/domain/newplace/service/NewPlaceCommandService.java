335c713c76b2f7dcfe39f8c9d92c8b631807e31d
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-07-21 13:42:40 +0900

    [FEAT] 공간 등록 목록 조회 api (#49)
    ✨ Feat: 공간 등록 목록 조회 api

--- src/main/java/com/gongspot/project/domain/newplace/service/NewPlaceCommandService.java
+++ src/main/java/com/gongspot/project/domain/newplace/service/NewPlaceCommandService.java
@@ -1,9 +1,11 @@
 package com.gongspot.project.domain.newplace.service;
 
+import com.gongspot.project.common.code.PageResponse;
 import com.gongspot.project.domain.newplace.dto.NewPlaceRequestDTO;
 import com.gongspot.project.domain.newplace.dto.NewPlaceResponseDTO;
 
 public interface NewPlaceCommandService {
     NewPlaceResponseDTO createNewPlaceProposal(NewPlaceRequestDTO requestDTO);
     NewPlaceResponseDTO getProposal(Long proposalId);
+    PageResponse<NewPlaceResponseDTO> getProposals(int page, int size);
 }
