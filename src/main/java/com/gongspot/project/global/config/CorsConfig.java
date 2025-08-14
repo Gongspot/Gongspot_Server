62a447f0410b5896a5431eeda79072291940aaf0
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2025-08-14 20:15:59 +0900

    refactor: kakao 리다이렉트 url 수정 및 cors설정 추가

--- src/main/java/com/gongspot/project/global/config/CorsConfig.java
+++ src/main/java/com/gongspot/project/global/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:8080", "http://api.gongspot.site",
-                "https://api.gongspot.site", "http://localhost:5182", "http://3.36.128.64"));
+                "https://api.gongspot.site", "http://localhost:5182", "http://3.36.128.64"," https://gong-spot.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
